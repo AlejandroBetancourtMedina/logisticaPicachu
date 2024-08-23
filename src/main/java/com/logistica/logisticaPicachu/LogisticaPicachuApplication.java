4fef70b59477d3a8689c7f640cdc7272f701157b
Alejo092 <alejandroignacio.betancourt@gmail.com>
2024-08-22 20:05:42 -0400

    Configuracion .yml

---
+++ src/main/java/com/logistica/logisticaPicachu/LogisticaPicachuApplication.java
@@ -0,0 +1,13 @@
+package com.logistica.logisticaPicachu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaPicachuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaPicachuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistica/logisticaPicachu/LogisticaPicachuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistica.logisticaPicachu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaPicachuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
